af3970968f169eccf5a0762659115a7c1fde41fd
syava aprilia puspitasari <syafaaps3@gmail.com>
2024-09-19 22:30:09 +0700

    update praktikum-6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,19 +10,20 @@ class UserController extends Controller
 {
     public function index(){
         // tambah data user dengan Eloquent Model
-        $data = [
+        /*$data = [
             'username' => 'customer-1',
             'nama'  => 'Pelanggan',
             'password' => Hash::make('12345'),
             'level_id' => 4
         ];
         UserModel::insert($data); //tambahkan data ke tabel m_user
+        */
 
-        /*$data = [
+        $data = [
             'nama'  => 'Pelanggan Pertama',
         ];
         UserModel::where('username', 'customer-1')->update($data); //update data user
-       */
+       
 
       //coba akses model UserModel
         $user = UserModel::all(); //Ambil semua data dari tabel m_user
