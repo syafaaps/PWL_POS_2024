fca4ad1689292275be0d6e0cd97453be4e749487
syava aprilia puspitasari <syafaaps3@gmail.com>
2024-09-20 21:55:09 +0700

    Jobsheet 4 Praktikum 2.6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,37 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
+        //coba akses model UserModel
+        //$user = UserModel::all(); //Ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+
+         //JS 3
+                // tambah data user dengan Eloquent Model
+        /*$data = [
+            'username' => 'customer-1',
+            'nama'  => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+        ];
+        UserModel::insert($data); //tambahkan data ke tabel m_user
+        */
+
+        /*$data = [
+            'nama'  => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); //update data user
+       */
+
+        //JS 4 Prak 1
+        /*
+       $data = [
+        'level_id' => 2,
+        'username' => 'manager_tiga',
+        'nama' => 'Manager 3',
+        'password' => Hash::make('12345')
+       ];
+       UserModel::create($data);
+        */
 
         //JS 4 Prak 2.1 no 6
         // $user = UserModel::firstwhere('level_id', 1);
@@ -93,54 +124,69 @@ class UserController extends Controller
     // dd($user->isDirty());
 
     //JS 4 Prak 2.5 no 3
-    $user = UserModel::create( [
-        'username' => 'manager 11',
-        'nama' => 'Manager11',
-        'password' => Hash:: make('12345'),
-        'level_id' => 2,
-        ]);
+    // $user = UserModel::create( [
+    //     'username' => 'manager 11',
+    //     'nama' => 'Manager11',
+    //     'password' => Hash:: make('12345'),
+    //     'level_id' => 2,
+    //     ]);
         
-        $user->username = 'manager12';
+    //     $user->username = 'manager12';
         
+    //     $user->save();
+        
+    //     $user->wasChanged(); // true
+    //     $user->wasChanged('username'); // true
+    //     $user->wasChanged( ['username', 'level_id']); // true
+    //     $user->wasChanged('nama'); // false
+    //     dd($user->wasChanged(['nama', 'username'])); // true
+
+    //JS Prak 2.6 no 2 & 6 & 9
+    $user = UserModel::all();
+    return view('user', ['data' => $user]);
+
+    }
+
+public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+{
+    UserModel::create([
+        'username' => $request->username,
+        'nama' => $request->nama,
+        'password' => Hash::make('$request->password'),
+        'level_id' => $request->level_id
+    ]);
+    
+    return redirect('/user');
+}
+public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request){
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
+
         $user->save();
-        
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged( ['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        dd($user->wasChanged(['nama', 'username'])); // true
 
-        //JS 4 Prak 1
-        /*
-       $data = [
-        'level_id' => 2,
-        'username' => 'manager_tiga',
-        'nama' => 'Manager 3',
-        'password' => Hash::make('12345')
-       ];
-       UserModel::create($data);
-        */
-      
-      //coba akses model UserModel
-        //$user = UserModel::all(); //Ambil semua data dari tabel m_user
-        // return view('user', ['data' => $user]);
+        return redirect('/user');
+    }
 
-         //JS 3
-                // tambah data user dengan Eloquent Model
-        /*$data = [
-            'username' => 'customer-1',
-            'nama'  => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
-        ];
-        UserModel::insert($data); //tambahkan data ke tabel m_user
-        */
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
 
-        /*$data = [
-            'nama'  => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
-       */
-         
+        return redirect('/user');
     }
 }
