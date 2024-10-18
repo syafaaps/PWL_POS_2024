9efaa24bf95085565cee42f288753a0abf80a9cf
syava aprilia puspitasari <syafaaps3@gmail.com>
2024-10-18 08:00:15 +0700

    Jobsheet 8 Prak 2 - Export Data ke Excel

--- routes/web.php
+++ routes/web.php
@@ -112,6 +112,7 @@ Route::group(['prefix' => 'barang'], function(){
     Route::delete('/{id}', [BarangController::class, 'destroy']);
     Route::get('/import',[BarangController::class,'import']);
     Route::post('/import_ajax',[BarangController::class,'import_ajax']);
+    Route::get('/export_excel',[BarangController::class,'export_excel']);
 });
 });
 
