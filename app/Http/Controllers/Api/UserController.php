cd340d443afce4c10912971a30a135a85c8f1989
syava aprilia puspitasari <syafaaps3@gmail.com>
2024-11-06 11:04:40 +0700

    Jobsheet 10 Tugas Implementasi CRUD API pada tabel m_user, m_kategori, dan m_barang

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = UserModel::all();
+        return response()->json($users);
+    }
+    public function store(Request $request)
+    {
+        $user = UserModel::create($request->all());
+        return response()->json($user, 201);
+    }
+    public function show(UserModel $user)
+    {
+        return response()->json($user);
+    }
+    public function update(Request $request, UserModel $user)
+    {
+        $user->update($request->all());
+        return response()->json($user, 200);
+    }
+    public function destroy(UserModel $user)
+    {
+        $user->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
 use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\BarangController;
 
 //Login
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
@@ -24,3 +26,24 @@ Route::post('levels', [LevelController::class, 'store' ]);
 Route:: get('levels/{level}', [LevelController::class, 'show' ]);
 Route:: put('levels/{level}', [LevelController::class, 'update' ]);
 Route::delete('levels/{level}', [LevelController::class, 'destroy' ]);
+
+// User routes
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{user}', [UserController::class, 'show']);
+Route::put('users/{user}', [UserController::class, 'update']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
+
+// Kategori routes
+Route::get('kategoris', [KategoriController::class, 'index']);
+Route::post('kategoris', [KategoriController::class, 'store']);
+Route::get('kategoris/{kategori}', [KategoriController::class, 'show']);
+Route::put('kategoris/{kategori}', [KategoriController::class, 'update']);
+Route::delete('kategoris/{kategori}', [KategoriController::class, 'destroy']);
+
+// Barang routes
+Route::get('barangs', [BarangController::class, 'index']);
+Route::post('barangs', [BarangController::class, 'store']);
+Route::get('barangs/{barang}', [BarangController::class, 'show']);
+Route::put('barangs/{barang}', [BarangController::class, 'update']);
+Route::delete('barangs/{barang}', [BarangController::class, 'destroy']);
